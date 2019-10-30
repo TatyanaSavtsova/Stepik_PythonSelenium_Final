b128182cbb10384d80d09f223d4cf5c1c272480e
TatyanaSavtsova <tatyana.g.lynx@gmail.com>
2019-10-30 20:09:38 +0300

    Add tests for Product page

--- pages/locators.py
+++ pages/locators.py
@@ -3,8 +3,18 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-	
+
+
 class LoginPageLocators():
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")	
-	
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    SUCCESS_ALERT = (By.CSS_SELECTOR, ".alert-success:nth-child(1)")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_NAME_IN_ALERT = (By.CSS_SELECTOR, ".alert-success:nth-child(1) strong")
+    PRICE_ALERT = (By.CSS_SELECTOR, ".alert-info")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_PRICE_IN_ALERT = (By.CSS_SELECTOR, ".alert-info strong")
